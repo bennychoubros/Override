153605277ce9c4e07dad256387fd6767bf84d7d3
jgounand <joris@gounand.fr>
2020-02-15 16:55:38 +0100

    level07 done ✅

--- level07/Ressources/docker.sh
+++ level07/Ressources/docker.sh
@@ -2,6 +2,6 @@ mkdir /goinfre/${USER}/ 2> /dev/null
 mkdir /goinfre/${USER}/docker 2> /dev/null
 rm -rf  /Users/${USER}/Library/Containers/com.docker.docker
 ln -fs /goinfre/${USER}/docker ~/Library/Containers/com.docker.docker
-chmod 500 /goinfre/${USER}
-docker build -t i386/debian .
-docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -ti -v `pwd`/../..:/root i386/debian
+chmod 700 /goinfre/${USER}
+docker build -t i386/ubuntu .
+docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -ti -v `pwd`/../..:/root i386/ubuntu
